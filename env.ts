d3c6086435af65ec5a1c149700631ead23f784c7
Yingying Lu <20095278@mail.wit.ie>
2024-04-19 04:02:37 +0100

    Completed Fan out implementation - added emailing logic

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const SES_REGION = 'eu-west-1';
+export const SES_EMAIL_FROM = '20095278@mail.wit.ie' ;
+export const SES_EMAIL_TO =  '1648268729@qq.com';
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -32,6 +32,10 @@ export class EDAAppStack extends cdk.Stack {
       displayName: "New Image topic",
     }); 
 
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
   // Lambda functions
 
   const processImageFn = new lambdanode.NodejsFunction(
@@ -45,15 +49,20 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+
  // S3 --> SQS
  imagesBucket.addEventNotification(
   s3.EventType.OBJECT_CREATED,
   new s3n.SnsDestination(newImageTopic)  // Changed
 );
 
-newImageTopic.addSubscription(
-  new subs.SqsSubscription(imageProcessQueue)
-);
+newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
 
  // SQS --> Lambda
   const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
@@ -61,11 +70,30 @@ newImageTopic.addSubscription(
     maxBatchingWindow: cdk.Duration.seconds(10),
   });
 
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(10),
+  }); 
+
   processImageFn.addEventSource(newImageEventSource);
+
+  mailerFn.addEventSource(newImageMailEventSource);
 
   // Permissions
 
   imagesBucket.grantRead(processImageFn);
+
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
 
     // Output
     
