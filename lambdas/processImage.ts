09b85082ad0ca18f02ad1ce84eb5dbf157036c07
Yingying Lu <20095278@mail.wit.ie>
2024-05-04 06:45:02 +0100

    Detect file type and throw exception

--- lambdas/processImage.ts
+++ lambdas/processImage.ts
@@ -13,27 +13,25 @@ const s3 = new S3Client();
 export const handler: SQSHandler = async (event) => {
   console.log("Event ", JSON.stringify(event));
   for (const record of event.Records) {
-    const recordBody = JSON.parse(record.body);  // Parse SQS message
-    const snsMessage = JSON.parse(recordBody.Message); // Parse SNS message
-
-    if (snsMessage.Records) {
-      console.log("Record body ", JSON.stringify(snsMessage));
-      for (const messageRecord of snsMessage.Records) {
+    const recordBody = JSON.parse(record.body);
+    console.log('Raw SNS message ',JSON.stringify(recordBody))
+    if (recordBody.Records) {
+      for (const messageRecord of recordBody.Records) {
         const s3e = messageRecord.s3;
         const srcBucket = s3e.bucket.name;
         // Object key may have spaces or unicode non-ASCII characters.
         const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
-        let origimage = null;
-        try {
-          // Download the image from the S3 source bucket.
-          const params: GetObjectCommandInput = {
-            Bucket: srcBucket,
-            Key: srcKey,
-          };
-          origimage = await s3.send(new GetObjectCommand(params));
-          // Process the image ......
-        } catch (error) {
-          console.log(error);
+        // Infer image type from file suffix.
+        const typeMatch = srcKey.match(/\.([^.]*)$/);
+        if (!typeMatch) {
+          console.log("Could not determine the image type.");
+          throw new Error("Could not determine the image type. ");
+        }
+        // Check if the image type is supported
+        const imageType = typeMatch[1].toLowerCase();
+        if (imageType != "jpeg" && imageType != "png") {
+          console.log(`Unsupported image type: ${imageType}`);
+          throw new Error("Unsupported image type: ${imageType. ");
         }
       }
     }
